802c2bc52050617886af67f4590c66f6434b0719
David Y. Stephenson <david@davidystephenson.com>
2023-03-17 17:22:58 +0100

    Use gameId prop correctly

--- src/view/Game.tsx
+++ src/view/Game.tsx
@@ -1,17 +1,13 @@
 import { Heading } from '@chakra-ui/react'
-import { useContext } from 'react'
-import { gameContext, GameReader } from '../reader/game'
+import { GameReader } from '../reader/game'
 import GameContentView from './GameContent'
 import GameWriters from './GameWriters'
 
 export default function GameView ({ gameId }: { gameId: string }): JSX.Element {
-  const gameState = useContext(gameContext)
   return (
     <GameReader gameId={gameId} DocView={GameContentView}>
       <Heading>
-        Game {gameState.id}
-        {' '}
-        <GameWriters />
+        Game {gameId} <GameWriters />
       </Heading>
     </GameReader>
   )
