66a0a3244d8fc0c3fbe3f2b11014387ccba5694b
David Y. Stephenson <david@davidystephenson.com>
2023-05-13 19:30:41 +0200

    Remove early play functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,29 +1,17 @@
-// import * as admin from "firebase-admin";
 import addGame from './cloudFunction/addGame'
 import joinGame from './cloudFunction/joinGame'
 import startGame from './cloudFunction/startGame'
 import onCreateUser from './cloudFunction/onCreateUser'
 import onDeleteUser from './cloudFunction/onDeleteUser'
-import playTrash from './cloudFunction/playTrash'
-import playPlay from './cloudFunction/playPlay'
-import playUntrash from './cloudFunction/playUntrash'
-import playUnplay from './cloudFunction/playUnplay'
 import playReady from './cloudFunction/playReady'
-import playUnready from './cloudFunction/playUnready'
 import deckChoose from './cloudFunction/deckChoose'
 import trashChoose from './cloudFunction/trashChoose'
 
-// process.env['FIRESTORE_EMULATOR_HOST'] = 'localhost:8080'
 exports.addGame = addGame
 exports.deckChoose = deckChoose
 exports.joinGame = joinGame
 exports.onCreateUser = onCreateUser
 exports.onDeleteUser = onDeleteUser
-exports.playPlay = playPlay
 exports.playReady = playReady
-exports.playTrash = playTrash
-exports.playUntrash = playUntrash
-exports.playUnplay = playUnplay
-exports.playUnready = playUnready
 exports.startGame = startGame
 exports.trashChoose = trashChoose
