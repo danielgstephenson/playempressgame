b89d89c6b0b60d90d568439587115bf6be8f4544
David Y. Stephenson <david@davidystephenson.com>
2023-06-29 19:09:12 +0200

    Tighten UI

--- src/view/Profile.tsx
+++ src/view/Profile.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading, Modal, ModalContent, ModalOverlay, Stack, useDisclosure } from '@chakra-ui/react'
+import { Box, Heading, Modal, ModalContent, ModalOverlay, useDisclosure } from '@chakra-ui/react'
 import PlayerReader from '../reader/player'
 import { useContext } from 'react'
 import { gameContext } from '../reader/game'
@@ -28,7 +28,7 @@ export default function ProfileView (): JSX.Element {
   }
   const deckFull = profileState.deckEmpty !== true
   const bidding = gameState.phase === 'auction'
-  const content = bidding ? <>({profileState.bid})</> : undefined
+  const content = bidding ? <>({profileState.bid} / {profileState.gold})</> : undefined
   return (
     <>
       <ChakraButton onClick={onOpen}>
@@ -40,26 +40,24 @@ export default function ProfileView (): JSX.Element {
         <Modal isOpen={isOpen} onClose={onClose} isCentered>
           <ModalOverlay />
           <ModalContent width='auto' onClick={onClose}>
-            <Stack direction='row'>
-              <Box>
-                <Heading size='md'>{profileState.displayName}</Heading>
-                <Status label='Bid' value={profileState.bid} />
-                <Status label='Gold' value={profileState.gold} />
-                <Status label='Silver' value={profileState.silver} />
-                <PublicTableauView />
-                <PublicTrashView />
-                <Heading size='sm'>Deck</Heading>
-                <Curtain open={deckFull}>
-                  <SmallSchemeView bg='gray.500' />
-                </Curtain>
-              </Box>
-              <Box>
-                <Heading size='sm'>Discard</Heading>
-                <ExpandedSchemeView
-                  rank={profileState.topDiscardScheme?.rank}
-                />
-              </Box>
-            </Stack>
+            <Box>
+              <Heading size='md'>{profileState.displayName}</Heading>
+              <Status label='Bid' value={profileState.bid} />
+              <Status label='Gold' value={profileState.gold} />
+              <Status label='Silver' value={profileState.silver} />
+              <PublicTableauView />
+              <PublicTrashView />
+              <Heading size='sm'>Deck</Heading>
+              <Curtain open={deckFull}>
+                <SmallSchemeView bg='gray.500' />
+              </Curtain>
+            </Box>
+            <Box>
+              <Heading size='sm'>Discard</Heading>
+              <ExpandedSchemeView
+                rank={profileState.topDiscardScheme?.rank}
+              />
+            </Box>
           </ModalContent>
         </Modal>
       </Curtain>
